7f7d496c35b57a8ddee9cd38662cdfec098070e8
bimal <bimal@192.168.0.103>
2020-02-23 01:09:05 +0530

    First commit

---
+++ Microservice-aws-Project/src/main/java/com/bimal/MicroserviceawsProject/MicroserviceAwsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bimal.MicroserviceawsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAwsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAwsProjectApplication.class, args);
+	}
+
+}
---
+++ Microservice-aws-Project/src/test/java/com/bimal/MicroserviceawsProject/MicroserviceAwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bimal.MicroserviceawsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
